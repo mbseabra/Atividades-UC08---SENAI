cc705c228f6323429a56edf99633a5eb1cf1a578
mbseabra <mrcsbgnt@gmail.com>
2022-11-14 15:12:05 -0300

    atividade atualizada

---
+++ atividade_5.js
@@ -0,0 +1,25 @@
+//Percorrer de Zero até o número de alunos
+
+//Se o número for par, escreva ‘par’ e o número correspondente
+
+//Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
+
+//Se o número for zero, escreva ‘zero’ e o número correspondente 
+
+let NumeroDeAlunos = 10
+
+for (let contador = 0; contador <= NumeroDeAlunos; contador++){
+   // console.log(contador)
+
+    if (contador == 0){
+        console.log("O número atual é zero")
+    }
+    else if (contador % 2 == 0){
+        console.log("O número " + contador + " é PAR")//concatenação
+    }
+    else{
+        console.log(`O número ${contador} é ÍMPAR`)//interpolação
+    }
+}
+
+
