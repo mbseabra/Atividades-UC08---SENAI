982277c1b42f7aae66b40e0f8393c74162890f38
mbseabra <mrcsbgnt@gmail.com>
2022-11-14 15:32:15 -0300

    segunda parte da aula atualizada

--- atividade_5.js
+++ atividade_5.js
@@ -5,7 +5,7 @@
 //Se o número for ímpar, escreva ‘ímpar’ e o número correspondente  
 
 //Se o número for zero, escreva ‘zero’ e o número correspondente 
-
+/*
 let NumeroDeAlunos = 10
 
 for (let contador = 0; contador <= NumeroDeAlunos; contador++){
@@ -20,6 +20,28 @@ for (let contador = 0; contador <= NumeroDeAlunos; contador++){
     else{
         console.log(`O número ${contador} é ÍMPAR`)//interpolação
     }
+}*/
+
+let NumeroDeAlunos = 10
+
+let contador = 0
+
+while (contador <= NumeroDeAlunos){
+   // console.log(contador)
+
+    if (contador == 0){
+        console.log("O número atual é zero")
+    }
+    else if (contador % 2 != 0){
+        console.log("O número " + contador + " é ÍMPAR")//concatenação
+    }
+    else{
+        console.log(`O número ${contador} é PAR`)//interpolação
+    }
+
+    contador++
 }
+
+
 
 
